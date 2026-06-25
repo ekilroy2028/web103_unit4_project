04b43c4810c024bc1e59e262b3606a829880a388
ekilroy2028 <ekilroy1@asu.edu>
2026-06-25 16:48:33 -0400

    complete bolt bucket car customizer
    Debugged: originally, the Save button or the Edit and Delete buttons were not working properly, so we went back to fix it. Also, the page drop-down as well

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import favicon from 'serve-favicon'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import carsRouter from './routes/cars.js'
 
 dotenv.config()
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'development') {
