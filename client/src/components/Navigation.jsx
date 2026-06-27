b0b5ad8380b735d3788ef7574b9a3c1f1c344c60
ekilroy2028 <ekilroy1@asu.edu>
2026-06-27 12:57:14 -0400

    update Navigation File for Sound

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { playClank } from '../utilities/sound.js'
 import '../App.css'
 import '../css/Navigation.css'
 
 const Navigation = () => {
+    const navigate = useNavigate()
+
+    const handleNav = (path) => {
+        playClank()
+        navigate(path)
+    }
+
     return (
         <nav>
             <ul>
                 <li><h1>Bolt Bucket 🏎️</h1></li>
             </ul>
-
             <ul>
-                <li><a href='/' role='button'>Customize</a></li>
-                <li><a href='/customcars' role='button'>View Cars</a></li>
+                <li><a onClick={() => handleNav('/')} role='button' style={{ cursor: 'pointer' }}>Customize</a></li>
+                <li><a onClick={() => handleNav('/customcars')} role='button' style={{ cursor: 'pointer' }}>View Cars</a></li>
             </ul>
-            
         </nav>
     )
 }
