49b3b69964b16b7f7915062a11c2ce7df282655c
ekilroy2028 <ekilroy1@asu.edu>
2026-06-27 13:16:44 -0400

    Add different sound debugging to save my page.

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,4 +1,3 @@
-keykilroy@keys-Mac-mini web103_unit4_project % cat client/src/components/Navigation.jsx
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { playClank } from '../utilities/sound.js'
@@ -19,11 +18,11 @@ const Navigation = () => {
                 <li><h1>Bolt Bucket 🏎️</h1></li>
             </ul>
             <ul>
-                <li><a onClick={() => handleNav('/')} role='button' style={{ cursor: 'pointer' }}>Customize</a></li>
-                <li><a onClick={() => handleNav('/customcars')} role='button' style={{ cursor: 'pointer' }}>View Cars</a></li>
+                <li><button className='nav-btn' onClick={() => handleNav('/')}>Customize</button></li>
+                <li><button className='nav-btn' onClick={() => handleNav('/customcars')}>View Cars</button></li>
             </ul>
         </nav>
     )
 }
 
-export default Navigation%                                                                 
+export default Navigation
